39de756a99e8f66efbe0810ffb928d56be664f37
100Daisy <100Daisy@gmail.com>
2021-02-25 10:37:45 +0100

    Revert "vibrator goes brr"
    This reverts commit 8e6da9f14432ff916c381d402742366797f22424.

--- device.mk
+++ device.mk
@@ -494,7 +494,8 @@ $(call inherit-product, build/target/product/verity.mk)
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.1-service.daisy
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
