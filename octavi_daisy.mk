cbc7319cac58326c44c4d9e67ad4fda8eb15fe26
100Daisy <48625180+100Daisy@users.noreply.github.com>
2021-03-05 09:43:13 +0100

    Update octavi_daisy.mk

--- octavi_daisy.mk
+++ octavi_daisy.mk
@@ -39,6 +39,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # Maintainer
 OCTAVI_BUILD_TYPE=Official
+OCTAVI_DEVICE_MAINTAINER := GitDaisy
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := daisy
